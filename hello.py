f090e0e5e36302480939f5fca0c8c3d8b2fad927
Ivan Krasilnikov <razer725@yandex.ru>
2020-03-09 14:08:36 +0300

    added gunicorn

---
+++ hello.py
@@ -0,0 +1,14 @@
+def app(environ, start_response):
+    """Simplest possible application object"""
+    # for i in environ['QUERY_STRING']:
+    if environ['QUERY_STRING']:
+        s = str(environ['QUERY_STRING']).split('&')
+        data = bytes('\n'.join(s), encoding='utf8')
+    else:
+        data = b''
+    status = '200 OK'
+    response_headers = [
+        ('Content-type', 'text/plain'),
+        ]
+    start_response(status, response_headers)
+    return iter([data])
